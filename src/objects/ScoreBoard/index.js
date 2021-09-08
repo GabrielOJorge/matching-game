0d5f271c27934ef1f4f79e7a36fec2bc696bc47c
GabrielOJorge <gabrieloliveiraj331@gmail.com>
2021-09-08 14:50:40 -0300

    Create the ScoreBoard object

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,6 +1,10 @@
+import PlayerName from "../../components/PlayerName";
+
 function ScoreBoard() {
   return /*html*/ `
-    <header class="">
+    <header class="score-board">
+      ${PlayerName('Player 1')}
+      ${PlayerName('Player 2')}
     </header>
   `;
 }
